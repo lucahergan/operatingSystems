02d7af292216c1dee2dfbeb1fd78fa718a7bbb4a
astro <astronomy487@gmail.com>
2025-02-06 12:44:49 -0600

    Update kprintf.c

--- projectThree/kprintf.c
+++ projectThree/kprintf.c
@@ -6,14 +6,18 @@ unsigned int  bufp;
 
 syscall kgetc()
 {
-    volatile struct ns16550_uart_csreg *regptr;
-    regptr = (struct ns16550_uart_csreg *)UART_BASE;
+	volatile struct ns16550_uart_csreg *regptr;
+	regptr = (struct ns16550_uart_csreg *)UART_BASE;
 
-    // TODO: First, check the unget buffer for a character.
-    //       Otherwise, check UART line status register, and
-    //       once there is data ready, get character c.
-
-    return SYSERR;
+	// First, check the unget buffer for a character.
+	// Otherwise, check UART line status register, and
+	// once there is data ready, get character c.
+	if (bufp) {
+		bufp--;
+		return ungetArray[bufp];
+	}
+	while (!(regptr->lsr & UART_LSR_DR));
+	return regptr->buffer;
 }
 
 /**
@@ -22,12 +26,12 @@ syscall kgetc()
  */
 syscall kcheckc(void)
 {
-    volatile struct ns16550_uart_csreg *regptr;
-    regptr = (struct ns16550_uart_csreg *)UART_BASE;
-
-    // TODO: Check the unget buffer and the UART for characters. pg 79 K&R
-
-	return SYSERR;
+	volatile struct ns16550_uart_csreg *regptr;
+	regptr = (struct ns16550_uart_csreg *)UART_BASE;
+	// Check the unget buffer and the UART for characters.
+	if (bufp) return 1;
+	if (regptr->lsr & UART_LSR_DR) return 1;
+	return 0;
 }
 
 /**
@@ -37,32 +41,32 @@ syscall kcheckc(void)
  */
 syscall kungetc(unsigned char c)
 {
-    // TODO: Check for room in unget buffer, put the character in or discard.
-    if (bufp < UNGETMAX) {
-        ungetArray[bufp++] = c;
-    }
-    return SYSERR;
+	// Check for room in unget buffer, put the character in or discard.
+	if (bufp < UNGETMAX) {
+		ungetArray[bufp] = c;
+		bufp++;
+		return c;
+	}
+	return SYSERR;
 }
+
 syscall kputc(uchar c)
 {
-    volatile struct ns16550_uart_csreg *regptr;
-    regptr = (struct ns16550_uart_csreg *)UART_BASE;
-
-    // TODO: Check UART line status register.
-    //       Once the Transmitter FIFO is empty, send character c.
-
-    regptr->buffer = 'H'; //Prints 'H'
-
-    return c;
+	volatile struct ns16550_uart_csreg *regptr;
+	regptr = (struct ns16550_uart_csreg *)UART_BASE;
+	// Check UART line status register.
+	// Once the Transmitter FIFO is empty, send character c.
+	while (!(regptr->lsr & UART_LSR_TEMT));
+	regptr->buffer = c;
+	return c;
 }
 
 syscall kprintf(const char *format, ...)
 {
-    int retval;
-    va_list ap;
-
-    va_start(ap, format);
-    retval = _doprnt(format, ap, (int (*)(long, long))kputc, 0);
-    va_end(ap);
-    return retval;
+	int retval;
+	va_list ap;
+	va_start(ap, format);
+	retval = _doprnt(format, ap, (int (*)(long, long))kputc, 0);
+	va_end(ap);
+	return retval;
 }
